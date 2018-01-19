8d42b5b91226785121d495b58f23b1902e0ad50c
unknown <nwy@xinhe.com>
2018-01-19 17:29:13 +0800

    豆果美食app初步搭建

---
+++ src/pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+      
+      <!-- 底部tabbar start-->
+      <tabbar>
+          <tabbar-item>
+              <img slot="icon" src="@/assets/logo.png">
+              <span slot="label">首页</span>
+          </tabbar-item>
+           <tabbar-item>
+              <img slot="icon" src="@/assets/logo.png">
+              <span slot="label">分类</span>
+          </tabbar-item>
+          <tabbar-item>
+              <img slot="icon" src="@/assets/logo.png">
+              <span slot="label">收藏</span>
+          </tabbar-item>
+      </tabbar>
+      <!-- 底部tabbar end-->
+  </div>
+</template>
+
+
+
+
+<script>
+import {Tabbar,TabbarItem} from 'vux'
+export default {
+    components:{
+        Tabbar,
+        TabbarItem
+    }
+  
+}
+</script>
+
